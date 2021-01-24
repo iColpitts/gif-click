8529e8b43521c4dea6061a0b331e7c1c6f01aa52
icolpitts <izzieccampbell@gmail.com>
2021-01-23 22:48:09 -0500

    more...

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ export default {
     ]),
     loadImg(options, callback) {
       var seconds = 0,
-          maxSeconds = 30,
+          maxSeconds = 60,
           complete = false,
           done = false;
 
