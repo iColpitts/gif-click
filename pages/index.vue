da72cf0ae5142b1417bdc36aedf8f66904445d6c
icolpitts <izzieccampbell@gmail.com>
2021-01-23 22:42:34 -0500

    lesser timeout

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ export default {
     ]),
     loadImg(options, callback) {
       var seconds = 0,
-          maxSeconds = 100,
+          maxSeconds = 20,
           complete = false,
           done = false;
 
