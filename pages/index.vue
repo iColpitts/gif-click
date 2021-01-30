e3d64e49847a81286f7c517c7cc49ae1e031fec3
icolpitts <izzieccampbell@gmail.com>
2021-01-30 17:33:13 -0500

    up time

--- pages/index.vue
+++ pages/index.vue
@@ -52,7 +52,7 @@ export default {
     ]),
     loadImg(options, callback) {
       var seconds = 0,
-        maxSeconds = 120,
+        maxSeconds = 150,
         complete = false,
         done = false;
 
